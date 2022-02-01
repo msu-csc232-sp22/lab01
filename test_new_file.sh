7aafad5ddae867c0fc1606e98963a7d6986da280
Jim Daehn <jdaehn@missouristate.edu>
2022-01-31 18:29:19 -0600

    Update new file creation test.

--- test_new_file.sh
+++ test_new_file.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-FILE=./src/Makefile
+FILE=./src/array.cpp
 if test -f "$FILE"; then
-    echo "$FILE exists. Task 2 completed successfully!"
+    echo "$FILE exists. Creation of a file named $FILE was successfully added to revision control..."
 else
-    echo "$FILE does not exist. You failed to add the $FILE to revision control..."
+    echo "$FILE does not exist. You failed to add a file named $FILE to revision control..."
     exit 1
 fi
